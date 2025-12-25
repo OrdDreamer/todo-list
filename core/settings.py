b72ad394f8754c453bf74595ddd9e9ff21ac7b10
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-12-25 18:02:34 +0100

    Fix flake8 errors

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6b8iy06#6bhj(3mn6qo&hez_kxn1t^!b!=mrx2cyc2@%rcjmvk"
+SECRET_KEY = ("django-insecure"
+              "-6b8iy06#6bhj(3mn6qo&hez_kxn1t^!b!=mrx2cyc2@%rcjmvk")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": ("django.contrib."
+                 "auth.password_validation.UserAttributeSimilarityValidator"),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": ("django.contrib."
+                 "auth.password_validation.MinimumLengthValidator"),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": ("django.contrib."
+                 "auth.password_validation.CommonPasswordValidator"),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": ("django.contrib."
+                 "auth.password_validation.NumericPasswordValidator"),
     },
 ]
 
